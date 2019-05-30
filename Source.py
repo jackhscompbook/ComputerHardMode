035c404c549296e0ed2659c747c1a35ae0179bd7
jackhscompbook <49289502+jackhscompbook@users.noreply.github.com>
2019-05-30 14:37:24 -0600

    Update Source.py

--- Source.py
+++ Source.py
@@ -1,12 +1,13 @@
+
 import time
 import random
 import pyautogui as pag
 from threading import Thread 
 import easygui as eg
 
-i = 0
+
 pag.FAILSAFE = False
-size = pyautogui.size()
+size = pag.size()
 
 if eg.ynbox('Are you ready to start?', 'Ready', \
             ('Yea, let\'s go', 'No thanks')) == False:
@@ -15,34 +16,39 @@ if eg.ynbox('Are you ready to start?', 'Ready', \
     exit()
 
 def main():
+    m = Thread(target=jitter)
+    m.start()
     t = Thread(target=start)
     t.start()
     time.sleep
     levels()
 
+def jitter():
+    i = 0
+    while True:
+        rx = random.randint(-25, 25)
+        ry = random.randint(-25, 25)
+        if i % 10 == 0:
+            pag.moveRel(ry, ry)
+        else:
+            pass
+        ++i
+        
 def start():
     pag.hotkey('alt', 'shiftleft', 'printscreen')
     time.sleep(0.125)
     pag.press('enter')
-
+    i = 2
     while i > -1:
         try:
-            rx = random.randint(-25, 25)
-            ry = random.randint(-25, 25)
-
+            
             ri = random.randint(-1,2)
             
             i += 1
 
-            ## this moves the mouse
-            if i % 10 == 0:
-                pag.moveRel(rx, ry)
-            else:
-                pass
-
 
             ## this presses arrow keys
-            if 29 % random.randint(1, 1001) == 0:
+            if 29 % random.randint(1, 1000001) == 0:
                 if ri == -1:
                     pag.press('left')
                     print('l')
@@ -66,38 +72,40 @@ def start():
 
             ## this presses alt-tab
 
-            if 29 % random.randint(1, 1000001) == 0:
+            if 29 % random.randint(1, 10000001) == 0:
                 pag.hotkey('alt', 'tab')
                 print('alt tab time bois')
 
             ## this presses Win.
 
-            if 29 % random.randint(1, 10001) == 0:
+            if 29 % random.randint(1, 10000001) == 0:
                 for i in range(10):
                     pag.hotkey('Win')
                     print('Win time bois')
 
             ## this presses Win+E
 
-            if 29 % random.randint(1, 10001) == 0:
+            if 29 % random.randint(1, 100000001) == 0:
                 for i in range(10):
                     pag.hotkey('win', 'e')
                     print('Win time bois')
 
             ## this presses the volume key
 
-            if 29 % random.randint(1, 1001) == 0:
+            if 29 % random.randint(1, 100000001) == 0:
+                print('Up time bois')
                 for i in range(100):
                     pag.press('volumeup')
-                    print('Up time bois')
 
             ## this clicks on things.
-            if 29 % random.randint(1, 10001) == 0:
-                pag.click(x=random.randint(0, size[0]), y=random.randint(0, size[1]))
+            if 29 % random.randint(1, 10000001) == 0:
+                pag.click(x=random.randint(0, size[0]), \
+                          y=random.randint(0, size[1]))
                 print('click clack')
 
-            if 29 % random.randint(1, 100001) == 0:
-                pag.drag(x=random.randint(0, size[0]), y=random.randint(0, size[1]))
+            if 29 % random.randint(1, 10000001) == 0:
+                pag.dragTo(x=random.randint(0, size[0]), \
+                         y=random.randint(0, size[1]))
                 print('get drugged')
 
         
@@ -106,14 +114,18 @@ def start():
             pass
 
 def levels():
-    eg.msgbox(msg='To win level 1, click the ok button!', \
-              title='1', ok_button='OK')
-    if eg.boolbox('To win level 2, click the button which says "kjbkbjgf".', \
-               '2', ['kjbkbjgf', 'kjdkbjgf']):
-        print('gg')
-    else:
-        print('how?')
-    eg.msgbox(msg='now that you\'ve done the levels, you get free play mode!')
+    while True:
+        eg.msgbox(msg='To win level 1, click the ok button!', \
+                  title='1', ok_button='OK')
+        if eg.boolbox('To win level 2, click the button which says \
+"kjbkbjgf".', '2', ['kjbkbjgf', 'kjdkbjgf']):
+            print('gg')
+            break
+        else:
+            print('how?')
+            continue
+    eg.msgbox(msg='now that you\'ve done the levels, \
+you get free play mode!')
 
-in __name__ == '__main__':
+if __name__ == '__main__':
     main()
